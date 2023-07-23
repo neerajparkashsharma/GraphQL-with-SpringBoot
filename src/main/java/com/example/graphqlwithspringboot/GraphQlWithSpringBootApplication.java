ef09543bd20fca06d5863ae90be3140ed97693b3
neerajparkashsharma <sharmanpsparkash@gmail.com>
2023-07-23 15:19:21 +0500

    initial commit

---
+++ src/main/java/com/example/graphqlwithspringboot/GraphQlWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graphqlwithspringboot/GraphQlWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
